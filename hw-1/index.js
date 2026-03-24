4e767ca32bc44f9c863f1bb669c6558f0ef12451
oleksandr vysotskyi <oleksandr.v.vysotskyi@gmail.com>
2026-03-24 10:27:42 +0200

    add hw_1

---
+++ hw-1/index.js
@@ -0,0 +1,46 @@
+let greeting = "Hello";
+let name = "Owu";
+let domain = "com";
+let country = "ua";
+let a1 = 1;
+let b1 = 10;
+let c1 = -999;
+let d = 123;
+const E = 3.14;
+const F = 2.7;
+const G = 16;
+const booleanT = true;
+const booleanF = false;
+console.log(
+  greeting,
+  name,
+  domain,
+  country,
+  a1,
+  b1,
+  c1,
+  d,
+  E,
+  F,
+  G,
+  booleanT,
+  booleanF
+);
+
+const firstName = "Oleksandr";
+const middleName = "Valeriyovych";
+const lastName = "Vysotskyi";
+let person = firstName + " " + middleName + " " + lastName;
+console.log(person);
+
+let a = 100;
+let b = "100";
+let c = true;
+console.log(typeof a, typeof b, typeof c);
+
+let getName = prompt("Enter your name");
+console.log(getName);
+let getMiddleName = prompt("Enter your middle name");
+console.log(getMiddleName);
+let getAge = prompt("Enter your age");
+console.log(getAge);
